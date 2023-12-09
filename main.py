061da5f6af4a3b5b104a9135f5a55672bae1acf9
Shira-tori <jiyuuomitometeirushiratori@protonmail.com>
2023-12-09 19:49:26 +0800

    added ascii art

--- main.py
+++ main.py
@@ -7,17 +7,28 @@ def chooseAnime(category):
     animes = category.parent.find_all('span', class_='js-title')
     synopsis = category.parent.find_all('p', class_='preline')
     rating = category.parent.find_all('span', class_='js-score')
+    date = category.parent.find_all('div', class_='info')
+
     rand_num = randrange(0, len(animes)-1)
+
     chosen_anime = animes[rand_num].text.strip()
     chosen_anime_synopsis = synopsis[rand_num].text.strip()
     chosen_anime_rating = rating[rand_num].text.strip()
-    print(f'Anime: {chosen_anime}\nRating: {chosen_anime_rating}\nSynopsis: {chosen_anime_synopsis}')
+    chosen_anime_date = date[rand_num].span.text.strip()
+    
+    print(f'Anime: {chosen_anime}')
+    print(f'Rating: {chosen_anime_rating}')
+    print(f'Date: {chosen_anime_date}')
+    print(f'Synopsis: {chosen_anime_synopsis}')
+
     input("Please press any key to continue...")
+
+    #TODO: Find a way to clear screen regardless of OS
     system("clear")
 
-if __name__ == "__main__":
-    system("clear")
+def seasonal_anime():
     url = 'https://myanimelist.net/anime/season'
+
     print("Loading...")
     response = requests.get(url)
     print("Done!\n\n")
@@ -45,6 +56,27 @@ if __name__ == "__main__":
                 exit(0)
             chooseAnime(categories[choice-1])
 
-        
     else:
         print("Bobo, wala ka atang internet.")
+
+def main_menu():
+    system("clear")
+    ascii_art = '''
+ /$$$$$$$$                            /$$$$$$$  /$$ /$$ /$$ /$$   /$$            /$$$$$$            /$$                        
+|__  $$__/                           | $$__  $$|__/| $$|__/| $$$ | $$           /$$__  $$          |__/                        
+   | $$  /$$$$$$   /$$$$$$   /$$$$$$ | $$  \ $$ /$$| $$ /$$| $$$$| $$  /$$$$$$ | $$  \ $$ /$$$$$$$  /$$ /$$$$$$/$$$$   /$$$$$$ 
+   | $$ |____  $$ /$$__  $$ |____  $$| $$$$$$$/| $$| $$| $$| $$ $$ $$ /$$__  $$| $$$$$$$$| $$__  $$| $$| $$_  $$_  $$ /$$__  $$
+   | $$  /$$$$$$$| $$  \ $$  /$$$$$$$| $$____/ | $$| $$| $$| $$  $$$$| $$  \ $$| $$__  $$| $$  \ $$| $$| $$ \ $$ \ $$| $$$$$$$$
+   | $$ /$$__  $$| $$  | $$ /$$__  $$| $$      | $$| $$| $$| $$\  $$$| $$  | $$| $$  | $$| $$  | $$| $$| $$ | $$ | $$| $$_____/
+   | $$|  $$$$$$$|  $$$$$$$|  $$$$$$$| $$      | $$| $$| $$| $$ \  $$|  $$$$$$$| $$  | $$| $$  | $$| $$| $$ | $$ | $$|  $$$$$$$
+   |__/ \_______/ \____  $$ \_______/|__/      |__/|__/|__/|__/  \__/ \____  $$|__/  |__/|__/  |__/|__/|__/ |__/ |__/ \_______/
+                  /$$  \ $$                                           /$$  \ $$                                                
+                 |  $$$$$$/                                          |  $$$$$$/                                                
+                  \______/                                            \______/                                                 
+         '''
+    print(ascii_art)
+    print("\n")
+
+if __name__ == '__main__':
+    main_menu()
+    seasonal_anime()
